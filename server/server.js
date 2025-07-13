bf51de9bb6d9c0dec11f88de33ba32444c0c8042
Anurag <anuragarwalkar@gmail.com>
2025-07-13 19:55:58 +0530

    Add build script and update static file path for production
    - Add "build" script to package.json for Vite production builds
    - Update server to serve static files from react-ui/dist instead of ui_backup

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static(path.join(__dirname, '../ui_backup'))); // Serve static files from UI folder
+app.use(express.static(path.join(__dirname, '../react-ui/dist'))); // Serve static files from UI folder
 
 // Import routes
 const jiraRoutes = require('./routes/jiraRoutes');
